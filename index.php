b8e9f9615d5bc3285555debdce38f0b7242b5868
bipinwaghmode <30909062+bipinwaghmode@users.noreply.github.com>
2017-08-11 18:57:47 +0530

    Basic drop down box
    I have created a basic drop down box without using database

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-
+<p>
+What is your Starting location?
+<select name="startLocation">
+  <option value="">Select...</option>
+  <option value="pune">Pune</option>
+  <option value="mumbai">Mumbai</option>
+</select>
+</p>
+<p>
+What is your Destination?
+<select name="destination">
+  <option value="">Select...</option>
+  <option value="pune">Pune</option>
+  <option value="mumbai">Mumbai</option>
+</select>
+</p>
